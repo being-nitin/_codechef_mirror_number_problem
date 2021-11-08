8d42cbb50a80cb84ce519160f84ba4011590ecb6
Nitin <nsinghboss02@gmail.com>
2021-11-08 09:46:15 +0530

    Initial commit

---
+++ src/com/nitincodes/Main.java
@@ -0,0 +1,18 @@
+package com.nitincodes;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	/* Write a program that accepts a number,n,and outputs the same.
+       Input
+       The only line contains a single integer.
+
+       Output
+       Output the answer in a single line.
+
+	 */
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+        System.out.println(n);
+    }
+}
